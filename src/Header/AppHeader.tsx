a1b87dc1b1395a953870f799ecdbcbb5811311b9
shriram sapparad <shriramcs@gmail.com>
2022-12-04 19:47:21 +0100

    fix: color fix

--- src/Header/AppHeader.tsx
+++ src/Header/AppHeader.tsx
@@ -1,7 +1,7 @@
 function AppHeader(){
     return (
         <header>
-            <h2 className="text-3xl font-bold underline text-blue-600 m-2">
+            <h2 className="text-3xl font-bold text-indigo-500 m-2">
                 TODO App
             </h2>
         </header>
